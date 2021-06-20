73651cf1c3877671c33645003f9c5859dd3bf31c
Plakzl25 <kaktusplay2006@gmail.com>
2021-06-20 14:41:38 +0300

    dz

---
+++ project1/project1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	bool isRaing = true;
+	if (isRaing == true) {
+		std::cout << "Raining";
+	}
+	else {
+		std::cout << "Not Raining";
+	}
+	/*
+	std:cout «"Hello World";
+	*/
+std:cout << "\n\n —--\n\n";
+	system("pause");
+	return 42;
+
+
+}
